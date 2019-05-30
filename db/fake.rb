7a6f08e000cffe12f469ef6678bbb7379c37760a
MrBloodness <morrowind4@inbox.lv>
2019-05-30 17:13:59 +0300

    refactoring

--- db/fake.rb
+++ db/fake.rb
@@ -3,21 +3,25 @@ employee_states = ['Active', 'Former', 'Potential']
 document_categories = ['Agreement', 'Offer', 'Invoice']
 document_states = ['Open', 'Closed']
 
-2.times do
-  User.create! do |u|
-    u.first_name = Faker::Name.first_name
-    u.last_name = Faker::Name.last_name
-    email_name = "#{u.first_name}_#{u.last_name}".parameterize('_')
-    email_host = ['inbox.lv', 'gmail.com'].sample
-    u.email = "#{email_name}@#{email_host}"
-    u.password = 'password'
-    u.password_confirmation = 'password'
+unless User.exists?
+  2.times do
+    User.create! do |u|
+      u.first_name = Faker::Name.first_name
+      u.last_name = Faker::Name.last_name
+      email_name = "#{u.first_name}_#{u.last_name}".parameterize('_')
+      email_host = ['inbox.lv', 'gmail.com'].sample
+      u.email = "#{email_name}@#{email_host}"
+      u.password = 'password'
+      u.password_confirmation = 'password'
+    end
   end
 end
 
-employee_states.each do |esa|
-  EmployeeState.create! do |es|
-    es.value = esa
+unless EmployeeState.exists?
+  employee_states.each do |esa|
+    EmployeeState.create! do |es|
+      es.value = esa
+    end
   end
 end
 
@@ -27,20 +31,25 @@ end
 #   end
 # end
 
-4.times do
-  Employee.create! do |e|
-    e.first_name = Faker::Name.first_name
-    e.last_name = Faker::Name.last_name
-    e.employee_state = EmployeeState.all.sample
-    email_name = "#{e.first_name}_#{e.last_name}".parameterize('_')
-    email_host = ['inbox.lv', 'gmail.com'].sample
-    e.email = "#{email_name}@#{email_host}"
+unless Employee.exists?
+  4.times do
+    Employee.create! do |e|
+      e.first_name = Faker::Name.first_name
+      e.last_name = Faker::Name.last_name
+      e.employee_state = EmployeeState.all.sample
+      e.employed_since = Faker::Date.between(Date.two_years_ago, Date.yesterday)
+      email_name = "#{e.first_name}_#{e.last_name}".parameterize('_')
+      email_host = ['inbox.lv', 'gmail.com'].sample
+      e.email = "#{email_name}@#{email_host}"
+    end
   end
 end
 
-document_categories.each do |dca|
-  DocumentCategory.create! do |dc|
-    dc.value = dca
+unless DocumentCategory.exists?
+  document_categories.each do |dca|
+    DocumentCategory.create! do |dc|
+      dc.value = dca
+    end
   end
 end
 
@@ -50,9 +59,11 @@ end
 #   end
 # end
 
-document_states.each do |dsa|
-  DocumentState.create! do |ds|
-    ds.value = dsa
+unless DocumentState.exists?
+  document_states.each do |dsa|
+    DocumentState.create! do |ds|
+      ds.value = dsa
+    end
   end
 end
 
@@ -62,23 +73,27 @@ end
 #   end
 # end
 
-4.times do
-  Document.create! do |d|
-    d.number = Faker::Number.decimal_part(3)
-    d.title = Faker::DcComics.title
-    d.description = Faker::Lorem.paragraph
-    d.user_id = User.all.sample.id
-    d.employee_id = Employee.all.sample.id
-    d.document_category = DocumentCategory.all.sample
-    d.document_state = DocumentState.all.sample
-    d.document_date = Date.today
-    d.deadline_date = Faker::Date.between(Date.tomorrow, 10.days.from_now)
+unless Document.exists?
+  4.times do
+    Document.create! do |d|
+      d.number = Faker::Number.decimal_part(3)
+      d.title = Faker::DcComics.title
+      d.description = Faker::Lorem.paragraph
+      d.user_id = User.all.sample.id
+      d.employee_id = Employee.all.sample.id
+      d.document_category = DocumentCategory.all.sample
+      d.document_state = DocumentState.all.sample
+      d.document_date = Date.today
+      d.deadline_date = Faker::Date.between(Date.tomorrow, 10.days.from_now)
+    end
   end
 end
 
-work_log_types.each do |wlt|
-  WorkLogType.create! do |wt|
-    wt.value = wlt
+unless WorkLogType.exists?
+  work_log_types.each do |wlt|
+    WorkLogType.create! do |wt|
+      wt.value = wlt
+    end
   end
 end
 
@@ -88,12 +103,14 @@ end
 #   end
 # end
 
-2.times do
-  WorkLog.create! do |w|
-    w.employee_id = Employee.all.sample.id
-    w.work_log_type = WorkLogType.all.sample
-    w.spent_time = Faker::Number.between(1, 8)
-    w.comment = Faker::Lorem.sentence
-    w.log_date = Faker::Date.forward(5)
+unless WorkLog.exists?
+  2.times do
+    WorkLog.create! do |w|
+      w.employee_id = Employee.all.sample.id
+      w.work_log_type = WorkLogType.all.sample
+      w.spent_time = Faker::Number.between(1, 8)
+      w.comment = Faker::Lorem.sentence
+      w.log_date = Faker::Date.forward(5)
+    end
   end
 end
